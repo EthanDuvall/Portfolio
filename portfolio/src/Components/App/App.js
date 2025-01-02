c4daf901be6a01f57ffb729b4fe9f14f99e1949b
Ethan <eduvall23@gmail.com>
2025-01-01 22:35:38 -0700

    Create Tabs For header

---
+++ portfolio/src/Components/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <button>Home</button>
+          <button>About</button>
+          <button>Services</button>
+          <button>Portfolio</button>
+          <button>Conact Me</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
