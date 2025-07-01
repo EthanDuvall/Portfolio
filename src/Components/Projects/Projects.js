62fb06a05ca10c6eef56d7eed6214ceaf099f370
Ethan <eduvall23@turing.edu>
2025-07-01 11:19:26 -0600

    More info now working and styled. Need to add more projects and fix bug with more info on right buttons

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -19,7 +19,7 @@ function Projects() {
   return (
     <div className="projects-holder">
       <div className={`project ${isInfo[0] ? "expanded" : ""}`}>
-        <aside>
+        <aside className="project-desc">
           <h2>Current project: Genshin Team Builder - Solo Project</h2>
           <p>
             Started/Starting :
@@ -33,42 +33,43 @@ function Projects() {
             Genshin Dev API and a smooth UI built in React.
           </p>
         </aside>
-        {isInfo[0] && (
-          <ul>
-            <h3>Planned features:</h3>
-            <li>A pop up to add and select which charters you own</li>
-            <li>
-              Having the team options include filters to decide wich kind of
-              team would you like for that current charter
-            </li>
-            <li>
-              Possiblity of allowing the auto import which charters you own
-              insted of manually adding them
-            </li>
-            <li>
-              Possiblity of adding weapons and which artifact set to use with
-              the charters
-            </li>
-            <li>
-              Possible future data base and api to reduce strain on the
-              application itself
-            </li>
-          </ul>
-        )}
+
         <div className="button-holder">
           <Link>Link is ( WIP ) </Link>
           <button
+            className="info-button"
             onClick={() => {
               toggleInfo(0);
-              console.log();
             }}
           >
             {isInfo[0] ? <p>Less info</p> : <p> More Info </p>}
           </button>
         </div>
       </div>
+      {isInfo[0] && (
+        <ul>
+          <h3>Planned features:</h3>
+          <li>A pop up to add and select which charters you own</li>
+          <li>
+            Having the team options include filters to decide wich kind of team
+            would you like for that current charter
+          </li>
+          <li>
+            Possiblity of allowing the auto import which charters you own insted
+            of manually adding them
+          </li>
+          <li>
+            Possiblity of adding weapons and which artifact set to use with the
+            charters
+          </li>
+          <li>
+            Possible future data base and api to reduce strain on the
+            application itself
+          </li>
+        </ul>
+      )}
       <div className={`project ${isInfo[1] ? "expanded" : ""}`}>
-        <aside>
+        <aside className="project-desc">
           <h2>Brain Defrost - Group Project</h2>
           <p>Time to complete 40 hours</p>
           <h3>
@@ -84,36 +85,11 @@ function Projects() {
             game stats via email!
           </p>
         </aside>
-        {isInfo[1] && (
-          <ul>
-            <h3>Current Features:</h3>
-            <li>
-              Using Action Cable added Webosockets for realtime and live updates
-              for every player
-            </li>
-            <li>
-              Using OpenAi API users are able to create a triva game based off
-              any prompt given
-            </li>
-            <li>
-              The user can request an email of the game stats at the end of the
-              game
-            </li>
-            <li>
-              At the end of each round the user will see a results showing whos
-              in the lead aswell as who got the most recent question right
-            </li>
-            <li>
-              The user receives a sharable link to send to other players
-              enabling them to join the triva game
-            </li>
-            <h3>Planned Features:</h3>
-            <li>No future features planned currently</li>
-          </ul>
-        )}
+
         <div className="button-holder">
           <Link to="https://brain-defrost.netlify.app">Deployed Link</Link>
           <button
+            className="info-button"
             onClick={() => {
               toggleInfo(1);
             }}
@@ -122,8 +98,35 @@ function Projects() {
           </button>
         </div>
       </div>
+      {isInfo[1] && (
+        <ul>
+          <h3>Current Features:</h3>
+          <li>
+            Using Action Cable added Webosockets for realtime and live updates
+            for every player
+          </li>
+          <li>
+            Using OpenAi API users are able to create a triva game based off any
+            prompt given
+          </li>
+          <li>
+            The user can request an email of the game stats at the end of the
+            game
+          </li>
+          <li>
+            At the end of each round the user will see a results showing whos in
+            the lead aswell as who got the most recent question right
+          </li>
+          <li>
+            The user receives a sharable link to send to other players enabling
+            them to join the triva game
+          </li>
+          <h3>Planned Features:</h3>
+          <li>No future features planned currently</li>
+        </ul>
+      )}
       <div className={`project  ${isInfo[2] ? "expanded" : ""}`}>
-        <aside>
+        <aside className="project-desc">
           <h2>Spotify Playlist Creator - Solo Project</h2>
 
           <p>Time to complete 40 hours</p>
@@ -136,38 +139,11 @@ function Projects() {
             chooses
           </p>
         </aside>
-        {isInfo[2] && (
-          <ul>
-            <h3>Current Features:</h3>
-            <li>
-              Uses the Spotify Web API to fetch available genres, allowing users
-              to select a genre as the basis for their playlist.
-            </li>
-            <li>
-              The home page displays the user's playlists, their current top
-              five songs, and their profile picture.
-            </li>
-            <li>
-              All playlists and songs displayed link directly to their Spotify
-              counterparts.
-            </li>
-            <li>
-              Playlists are automatically uploaded to the user's library upon
-              creation.
-            </li>
 
-            <h3>Planned Features:</h3>
-            <li>Enable users to remove songs from playlists within the app.</li>
-            <li>Allow users to add a custom image to their playlists.</li>
-            <li>
-              Explore additional playlist creation options beyond genre
-              selection.
-            </li>
-          </ul>
-        )}
         <div className="button-holder">
           <Link to="https://playlist-creator.vercel.app">Deployed Link</Link>
           <button
+            className="info-button"
             onClick={() => {
               toggleInfo(2);
             }}
@@ -176,6 +152,34 @@ function Projects() {
           </button>
         </div>
       </div>
+      {isInfo[2] && (
+        <ul>
+          <h3>Current Features:</h3>
+          <li>
+            Uses the Spotify Web API to fetch available genres, allowing users
+            to select a genre as the basis for their playlist.
+          </li>
+          <li>
+            The home page displays the user's playlists, their current top five
+            songs, and their profile picture.
+          </li>
+          <li>
+            All playlists and songs displayed link directly to their Spotify
+            counterparts.
+          </li>
+          <li>
+            Playlists are automatically uploaded to the user's library upon
+            creation.
+          </li>
+
+          <h3>Planned Features:</h3>
+          <li>Enable users to remove songs from playlists within the app.</li>
+          <li>Allow users to add a custom image to their playlists.</li>
+          <li>
+            Explore additional playlist creation options beyond genre selection.
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
